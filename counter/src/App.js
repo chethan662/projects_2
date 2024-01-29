23cb973ee8b5a10823f2f9505b59475486c6292a
chethan662 <chethanlokku@gmail.com>
2024-01-29 23:24:54 +0530

    .

---
+++ counter/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import {useState} from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
+  const decrement = () => {
+    setCount(prevCount => prevCount - 1);
+  };
+
+  return (
+    <div className="counter">
+      <h1>Counter App</h1>
+      <div className="counter">Count: {count}</div>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
